2913c2ffebe0c6b7ab22cb30b164297ba725f563
seetaramkumark <seetaramkumarkollisetty3342@gmail.com>
2024-07-21 11:26:35 +0530

    change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
                 <img src={assets.logv} alt="" />
                 <p>Delight Foods brings to you a collection of carefully selected foods from India. In our passionate search to offer food that is unique and full of flavor, we have ensured you get only the best and most authentic regional food products.</p>
                 <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
+                    <img src={assets.facebook_icon} alt="#" />
+                    <img src={assets.twitter_icon} alt="#" />
+                    <img src={assets.linkedin_icon} alt="https://www.linkedin.com/in/kolliseti-seetaram-kumar-b37382247/" />
                 </div>
             </div>
             <div className="footer-content-center">
@@ -33,7 +33,7 @@ const Footer = () => {
             </div>
         </div>
         <hr />
-        <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+        <p className="footer-copyright">Copyright 2024 © Delight Foods.com - All Right Reserved.</p>
     </div>
   )
 }
